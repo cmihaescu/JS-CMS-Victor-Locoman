057fbaa266aa38a13e94f7643610cddb5906333a
unknown <cornu_bv@yahoo.com>
2020-11-02 18:31:13 +0200

    more updates

--- src/public/home.js
+++ src/public/home.js
@@ -1,22 +1,31 @@
-let numberOfArticles =document.getElementsByTagName("h2").length
+// const { connect } = require("mongodb");
+
+const numberOfArticles =document.getElementsByTagName("h2").length
 
 for (let i=0; i<numberOfArticles;i++){
-let scrollToArticleLinkId = "scroll-to-post-"+i;
-let targetId = "post-"+i;
-let goBackButtonClass = "post-btn-"+i;
-let target = document.querySelector(`#${targetId}`)
-let scrollToArticleLink = document.querySelector(`#${scrollToArticleLinkId}`);
-let goBackButton = document.querySelector(`.${goBackButtonClass}`)
-let goBackTarget = document.querySelector('#h1')
+    let scrollToArticleLinkId = "scroll-to-post-"+i;
+    let targetId = "post-"+i;
+    let goBackButtonClass = "post-btn-"+i;
+    let target = document.querySelector(`#${targetId}`)
+    let scrollToArticleLink = document.querySelector(`#${scrollToArticleLinkId}`);
+    let goBackButton = document.querySelector(`.${goBackButtonClass}`)
+    let goBackTarget = document.querySelector('#h1')
 
-function smoothScroll(){
-    target.scrollIntoView({behavior: "smooth", block: "start"});
-    }
+    function smoothScroll(){
+        console.log('merge scroll to post')
+        target.scrollIntoView({behavior: "smooth", block: "start"});
+        console.log("numberOfArticles:", numberOfArticles)
 
-function smoothScrolltoTop(){
-    goBackTarget.scrollIntoView({behavior: "smooth", block: "start"});
-    }
+        }
 
-scrollToArticleLink.addEventListener("click", smoothScroll);
-goBackButton.addEventListener("click", smoothScrolltoTop);
+    function smoothScrolltoTop(){
+        console.log('merge scroll to top')
+        goBackTarget.scrollIntoView({behavior: "smooth", block: "start"});
+        console.log("numberOfArticles:", numberOfArticles)
+
+        }
+    
+
+    scrollToArticleLink.addEventListener("click", smoothScroll);
+    goBackButton.addEventListener("click", smoothScrolltoTop);
 }
