cfb7518cd52b09a992c7694a204eef6b3e09deef
unknown <cornu_bv@yahoo.com>
2020-11-02 10:01:34 +0200

    scroll buttons work

--- src/public/home.js
+++ src/public/home.js
@@ -1,3 +1,22 @@
-let title = document.querySelector('#post-4');
+let numberOfArticles =document.getElementsByTagName("h2").length
 
-title.scrollIntoView()
+for (let i=0; i<numberOfArticles;i++){
+let scrollToArticleLinkId = "scroll-to-post-"+i;
+let targetId = "post-"+i;
+let goBackButtonClass = "post-btn-"+i;
+let target = document.querySelector(`#${targetId}`)
+let scrollToArticleLink = document.querySelector(`#${scrollToArticleLinkId}`);
+let goBackButton = document.querySelector(`.${goBackButtonClass}`)
+let goBackTarget = document.querySelector('#h1')
+
+function smoothScroll(){
+    target.scrollIntoView({behavior: "smooth", block: "start"});
+    }
+
+function smoothScrolltoTop(){
+    goBackTarget.scrollIntoView({behavior: "smooth", block: "start"});
+    }
+
+scrollToArticleLink.addEventListener("click", smoothScroll);
+goBackButton.addEventListener("click", smoothScrolltoTop);
+}
