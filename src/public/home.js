796c6c97e9e7a3287ef11037ed5b693299083095
unknown <cornu_bv@yahoo.com>
2020-11-10 07:07:52 +0200

    scroll working

--- src/public/home.js
+++ src/public/home.js
@@ -1,31 +1,23 @@
-// const { connect } = require("mongodb");
-
 const numberOfArticles =document.getElementsByTagName("h2").length
+const goBackButton= document.querySelectorAll(".go-back-up");
+const goBackTarget = document.querySelector("#h1");
+const articleLinkBtn = document.querySelectorAll(".scroll-to-post")
 
-for (let i=0; i<numberOfArticles;i++){
-    let scrollToArticleLinkId = "scroll-to-post-"+i;
-    let targetId = "post-"+i;
-    let goBackButtonClass = "post-btn-"+i;
-    let target = document.querySelector(`#${targetId}`)
-    let scrollToArticleLink = document.querySelector(`#${scrollToArticleLinkId}`);
-    let goBackButton = document.querySelector(`.${goBackButtonClass}`)
-    let goBackTarget = document.querySelector('#h1')
+function smoothScrolltoTop(){
+    goBackTarget.scrollIntoView({behavior: "smooth", block: "start"});
+    }
 
-    function smoothScroll(){
-        console.log('merge scroll to post')
-        target.scrollIntoView({behavior: "smooth", block: "start"});
-        console.log("numberOfArticles:", numberOfArticles)
+for (let article of articleLinkBtn) {
+    article.addEventListener('click', function smoothScrolltoArticle(){
+        let targetArticle = document.querySelector(`#article-${article.id}`)
+        targetArticle.scrollIntoView({behavior: "smooth", block: "start"});
+        })
+    }
 
-        }
-
-    function smoothScrolltoTop(){
-        console.log('merge scroll to top')
-        goBackTarget.scrollIntoView({behavior: "smooth", block: "start"});
-        console.log("numberOfArticles:", numberOfArticles)
-
-        }
-    
-
-    scrollToArticleLink.addEventListener("click", smoothScroll);
-    goBackButton.addEventListener("click", smoothScrolltoTop);
+function addEventListenerList(list, event, fn) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].addEventListener(event, fn, false);
+    }
 }
+
+addEventListenerList(goBackButton, "click", smoothScrolltoTop);
